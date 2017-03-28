be3a9be6b506b1efc19eef8f3ccb148fa91950e2
STEVENSON Sean R <s016850f@student.staffs.ac.uk>
2017-03-28 10:15:48 +0100

    updated files

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,41 @@
 var CACHE_NAME = 'gih-cache-v5';
 var CACHED_URLS = [
   // Our HTML
+  'browserconfig.xml'
   'first.html',
+  'index.html',
+  'manifest.JSON',
+  'mystyles.css',
+  'offline.html',
+  'README.MD',
+  'second.html',
+  'serviceworker.js',
+  'styles.css',
+  'test_manifest.html',
+  'test_start_materialize'
+  'appImages/dino.png',
+  'appImages/favicon.icon',
+  'appImages/favicon-16x16.png',
+  'appImages/favicon-32x32.png',
+  'appImages/favicon-96x96.png',
+  'appImages/jack.png',
+  'appImages/paddy.png',
+  'eventImages/android-icon-36x36.png',
+  'eventImages/android-icon-48x48.png',
+  'eventImages/android-icon-72x72.png',
+  'eventImages/android-icon-96x96.png',
+  'eventImages/android-icon-144x144.png',
+  'eventImages/android-icon-192x192.png',
+  'eventImages/example-blog01.jpg',
+  'eventImages/example-blog02.jpg',
+  'eventImages/example-blog03.jpg',
+  'eventImages/example-blog04.jpg',
+  'eventImages/example-blog05.jpg',
+  'eventImages/example-blog06.jpg',
+  'eventImages/example-blog07.jpg',
+  'eventImages/example-blog08.jpg',
+  'eventImages/example-blog09.jpg',
+  
   // Stylesheets and fonts
   // JavaScript
   // Images
