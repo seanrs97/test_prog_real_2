a8a3e50729d7109c622ee597afb56e06831ce68f
STEVENSON Sean R <s016850f@student.staffs.ac.uk>
2017-03-28 11:02:54 +0100

    new files

--- serviceworker.js
+++ serviceworker.js
@@ -86,7 +86,7 @@ self.addEventListener('activate', function(event) {
     caches.keys().then(function(cacheNames) {
       return Promise.all(
         cacheNames.map(function(cacheName) {
-          if (cacheName.startsWith('gih-cache-v5') && CACHE_NAME !== cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
             return caches.delete(cacheName);
           }
         })
