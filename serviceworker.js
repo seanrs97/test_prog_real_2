6b3a1ae721b5563055b4855fa24e257969889621
STEVENSON Sean R <s016850f@student.staffs.ac.uk>
2017-03-28 10:03:01 +0100

    new files

---
+++ serviceworker.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'gih-cache-v5';
+var CACHED_URLS = [
+  // Our HTML
+  'first.html',
+  // Stylesheets and fonts
+  // JavaScript
+  // Images
+];
+
+self.addEventListener('install', function(event) {
+  // Cache everything in CACHED_URLS. Installation will fail if something fails to cache
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHED_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('first.html');
+        }
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName.startsWith('gih-cache') && CACHE_NAME !== cacheName) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
+
